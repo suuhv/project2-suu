2f3408acd0fd93d69e33780c4fd4fb3dd49b1cb6
Suu Ha <havansuu29111995@gmail.com>
2017-07-06 08:58:47 +0700

    init-model, add relationship

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  belongs_to :user
+
+  has_many :comments, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  validates :title, presence: true
+  validates :content, presence: true
+end
